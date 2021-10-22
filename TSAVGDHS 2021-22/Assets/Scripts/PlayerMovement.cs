6aeebd8c41f18492556cc585ca480173a15c0b8c
John Jorgensen <john@jorgensenfamily.us>
2021-10-22 13:46:38 -0700

    Almost working movement
    Speed doesn't change when you change the direction (might be fine, idk)

--- TSAVGDHS 2021-22/Assets/Scripts/PlayerMovement.cs
+++ TSAVGDHS 2021-22/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 	InputMaster controls;
-	public float speed;
+	public float maxSpeed;
+	public float acceleration;
+	public float deceleration;
+	float currentSpeed;
+	Vector2 lastDirection;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +23,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement();
-    }
-
-	void movement()
-	{
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+		if (Mouse.current.leftButton.isPressed)
+		{
+			currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration * Time.deltaTime);
+			Debug.Log("Speed: " + currentSpeed);
+			transform.position = Vector2.MoveTowards(transform.position, mousePos, currentSpeed * Time.deltaTime);
+			lastDirection = (mousePos - (Vector2)transform.position).normalized;
+		}
+		else
+		{
+			currentSpeed = Mathf.MoveTowards(currentSpeed, 0, deceleration * Time.deltaTime);
+			transform.Translate(lastDirection * currentSpeed * Time.deltaTime);
+		}
+		/*
 		Vector2 input = controls.Player.Movement.ReadValue<Vector2>();
 		Vector2 movement = input * speed * Time.deltaTime;
 		transform.Translate(movement);
+		*/
 	}
 }
