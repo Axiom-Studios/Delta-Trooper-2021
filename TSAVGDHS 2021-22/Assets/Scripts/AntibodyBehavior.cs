fe4dd2ee4e6572354b67ac34f799dd649be8732c
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-11-08 16:27:54 -0800

    Revert "Merge branch 'main' into basic-enemies"
    This reverts commit 1a3a94e2307f5eaa9fbee3fecfb27caa3daa4db7, reversing
    changes made to 7375f32b21c5afdf32c266bcb33c2268eec1517d.

---
+++ TSAVGDHS 2021-22/Assets/Scripts/AntibodyBehavior.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntibodyBehavior : MonoBehaviour
+{
+    public bool chasing = false;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private float speed = 7;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        sr = this.GetComponent<SpriteRenderer>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!chasing)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < 5)
+            {
+                chasing = true;
+                sr.color = Color.gray;
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) > 5)
+            {
+                chasing = false;
+                sr.color = Color.white;
+            }
+            else
+            {
+                // Go to player
+                Vector2 direction = (Vector2)player.transform.position - rb.position;
+                direction = direction.normalized;
+                Vector2 velocity = direction * speed * Time.fixedDeltaTime;
+
+                rb.MovePosition(rb.position + velocity);
+            }
+        }
+    }
+}
