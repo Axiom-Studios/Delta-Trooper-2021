b00b3cdff027ca88bb116b7e204e2c21d9258a21
Jakob Nacanaynay <63821647+j-nac@users.noreply.github.com>
2021-11-08 18:37:09 -0800

    Add back in John's movement code

--- TSAVGDHS 2021-22/Assets/Scripts/PlayerMovement.cs
+++ TSAVGDHS 2021-22/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
 	InputMaster controls;
     private Rigidbody2D rb;
+    public float maxSpeed = 10f;
+    public float acceleration = 8f;
+    public float deceleration = 5f;
+    float currentSpeed;
+    Vector2 lastDirection;
 	public float speed = 10;
 
     // Start is called before the first frame update
@@ -19,15 +25,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+		if (Mouse.current.leftButton.isPressed)
+		{
+			currentSpeed = Mathf.MoveTowards(currentSpeed, maxSpeed, acceleration * Time.fixedDeltaTime);
+			Debug.Log("Speed: " + currentSpeed);
+			rb.MovePosition(Vector2.MoveTowards(transform.position, mousePos, currentSpeed * Time.fixedDeltaTime));
+			lastDirection = (mousePos - (Vector2)transform.position).normalized;
+		}
+		else
+		{
+			currentSpeed = Mathf.MoveTowards(currentSpeed, 0, deceleration * Time.fixedDeltaTime);
+			rb.MovePosition(rb.position + lastDirection * currentSpeed * Time.fixedDeltaTime);
+		}
+		/*
+		Vector2 input = controls.Player.Movement.ReadValue<Vector2>();
+		Vector2 movement = input * speed * Time.deltaTime;
+		transform.Translate(movement);
+		*/
+	}
+    /*
+    void FixedUpdate()
+    {
         movement();
     }
 
 	void movement()
 	{
 		Vector2 input = controls.Player.Movement.ReadValue<Vector2>();
-		Vector2 movement = input * speed * Time.fixedDeltaTime;
-		rb.MovePosition(rb.position + movement);
+		Vector2 movement = input * acceleration * Time.fixedDeltaTime;
+		rb.velocity += movement;
 	}
+
+    */
 
     private void OnCollisionEnter2D(Collision2D other)
     {
